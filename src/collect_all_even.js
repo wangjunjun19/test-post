0dcf4e1585fb942754e3394759fa5dd55e0cf2af
wangjunjun19 <735276168@qq.com>
2016-05-22 18:01:21 +0800

    first commit

--- src/collect_all_even.js
+++ src/collect_all_even.js
@@ -1,6 +1,18 @@
 
 function collect_same_elements(collection_a, object_b) {
  //在此处写代码
+    var collection_c=[];
+    for(var i=0;i<collection_a.length;i++)
+    {
+        for(var j=0;j<object_b.value.length;j++)
+        {
+            if(collection_a[i].key===object_b.value[j])
+            {
+                collection_c.push(collection_a[i].key);
+            }
+        }
+    }
+    return collection_c;
 }
 
 module.exports = collect_same_elements;
